412c8ac7c2af4e811ebf5b9f2369a6c185adb792
James Cohenour <jam.coh1997@gmail.com>
2018-04-27 15:50:31 -0500

    Display added(?)
    added javascript for hopefully changin the visibility of the articles.

---
+++ Scripts.js
@@ -0,0 +1,16 @@
+var articles = new Array('Welcome', 'Sapiens', 'AriHun', 'Wild', 'Anger', 'Breath');
+
+function show(id)
+{
+    for(i=0; i < articles.length; i++)
+    {
+        if(articles[i] == id)
+        {
+            document.getElementById(articles[i]).style.display = 'block';
+        }
+        else
+        {
+            document.getElementById(articles[i]).style.display = 'none';
+        }
+    }
+}
