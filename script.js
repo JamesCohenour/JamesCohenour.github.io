1571ea3d4844f1e35a5c7a4b8707110947fa1571
James Cohenour <jam.coh1997@gmail.com>
2019-11-19 21:50:44 -0600

    close

--- script.js
+++ script.js
@@ -1,66 +1,169 @@
-JavaActivated = false;
-CActivated = false;
-PythonActivated = false;
-ResearchActivated = false;
+MazeActivated = false;
+DBActivated = false;
+DotaActivated = false;
+HashingActivated = false;
+SortingActivated = false;
+SudokuActivated = false;
+CheckersActivated = false;
+CameraActivated = false;
+RubyActivated = false;
+WebActivated = false;
 
 
-function displayJavaInfo()
+
+function displayMazeInfo()
 {
-    if(JavaActivated)
+    if(MazeActivated)
     {
-        ele = document.getElementById("Javainfo");
-        ele.innerHTML = "<h2>Java Projects<h2>";
-        JavaActivated = !JavaActivated;
+        ele = document.getElementById("MazeInfo");
+        ele.innerHTML = "<h2>Maze Solver<h2>";
+        MazeActivated = !MazeActivated;
     }
     else
     {
-        ele = document.getElementById("Javainfo");
+        ele = document.getElementById("MazeInfo");
         ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
-        JavaActivated = !JavaActivated;
+        MazeActivated = !MazeActivated;
     }
 }
-function displayCInfo()
+function displayDBInfo()
 {
-    if(CActivated)
+    if(DBActivated)
     {
-        ele = document.getElementById("Cinfo");
-        ele.innerHTML = "<h2>C/C++ Projects<h2>";
-        CActivated = !CActivated;
+        ele = document.getElementById("DBInfo");
+        ele.innerHTML = "<h2>Sample Database<h2>";
+        DBActivated = !DBActivated;
     }
     else
     {
-        ele = document.getElementById("Cinfo");
+        ele = document.getElementById("DBInfo");
         ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
-        CActivated = !CActivated;
+        DBActivated = !DBActivated;
     }
 }
-function displayPythonInfo()
+function displayDotaInfo()
 {
-    if(PythonActivated)
+    if(DotaActivated)
     {
-        ele = document.getElementById("Pythoninfo");
-        ele.innerHTML = "<h2>Python/Ruby Projects<h2>";
-        PythonActivated = !PythonActivated;
+        ele = document.getElementById("DotaInfo");
+        ele.innerHTML = "<h2>Dota 2 Information Bot<h2>";
+        DotaActivated = !DotaActivated;
     }
     else
     {
-        ele = document.getElementById("Pythoninfo");
+        ele = document.getElementById("DotaInfo");
         ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
-        PythonActivated = !PythonActivated;
+        DotaActivated = !DotaActivated;
     }
 }
-function displayResearchInfo()
+function displayHashingInfo()
 {
-    if(ResearchActivated)
+    if(HashingActivated)
     {
-        ele = document.getElementById("Researchinfo");
-        ele.innerHTML = "<h2>Research Paper<h2>";
-        ResearchActivated = !ResearchActivated;
+        ele = document.getElementById("HashingInfo");
+        ele.innerHTML = "<h2>Hashing Research Paper<h2>";
+        HashingActivated = !HashingActivated;
     }
     else
     {
-        ele = document.getElementById("Researchinfo");
+        ele = document.getElementById("HashingInfo");
         ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
-        ResearchActivated = !ResearchActivated;
+        HashingActivated = !HashingActivated;
+    }
+}
+
+function displaySortingInfo()
+{
+    if(SortingActivated)
+    {
+        ele = document.getElementById("SortingInfo");
+        ele.innerHTML = "<h2>Sorting Analysis<h2>";
+        SortingActivated = !SortingActivated;
+    }
+    else
+    {
+        ele = document.getElementById("SortingInfo");
+        ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
+        SortingActivated = !SortingActivated;
+    }
+}
+
+function displaySudokuInfo()
+{
+    if(SudokuActivated)
+    {
+        ele = document.getElementById("SudokuInfo");
+        ele.innerHTML = "<h2>Sudoku Game<h2>";
+        SudokuActivated = !SudokuActivated;
+    }
+    else
+    {
+        ele = document.getElementById("SudokuInfo");
+        ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
+        SudokuActivated = !SudokuActivated;
+    } 
+}
+
+function displayCheckersInfo()
+{
+    if(CheckersActivated)
+    {
+        ele = document.getElementById("CheckersInfo");
+        ele.innerHTML = "<h2>Mobile Checkers<h2>";
+        CheckersActivated = !CheckersActivated;
+    }
+    else
+    {
+        ele = document.getElementById("CheckersInfo");
+        ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
+        CheckersActivated = !CheckersActivated;
+    }
+}
+
+function displayCameraInfo()
+{
+    if(CameraActivated)
+    {
+        ele = document.getElementById("CameraInfo");
+        ele.innerHTML = "<h2>Motion Detecting Camera<h2>";
+        CameraActivated = !CameraActivated;
+    }
+    else
+    {
+        ele = document.getElementById("CameraInfo");
+        ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
+        CameraActivated = !CameraActivated;
+    }
+}
+
+function displayRubyInfo()
+{
+    if(RubyActivated)
+    {
+        ele = document.getElementById("RubyInfo");
+        ele.innerHTML = "<h2>History of Ruby<h2>";
+        RubyActivated = !RubyActivated;
+    }
+    else
+    {
+        ele = document.getElementById("RubyInfo");
+        ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
+        RubyActivated = !RubyActivated;
+    }
+}
+
+function displayWebInfo()
+{
+    if(WebActivated)
+    {
+        ele = document.getElementById("WebInfo");
+        ele.innerHTML = "<h2>Create Your Own Webpage!<h2>";
+        WebActivated = !WebActivated;
+    }
+    else
+    {
+        ele = document.getElementById("WebInfo");
+        ele.innerHTML += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
+        WebActivated = !WebActivated;
     }
 }
